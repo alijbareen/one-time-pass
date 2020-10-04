ffefd27593909fef575655105cbc6ee413d15a17
alijb <alijb@post.bgu.ac.il>
2020-10-04 20:48:47 +0300

    Create User

--- Firebase/functions/create_user.js
+++ Firebase/functions/create_user.js
@@ -1,3 +1,21 @@
+const admin = require("firebase-admin");
+
 module.exports = function (req, res) {
-  res.send(req.body);
+  //verify the user provided a phone
+  if (!req.body.phone) {
+    return res.status(422).send({ error: "bad Input" });
+  }
+
+  //format the phone number to remove dashes etc...
+  const phone = String(req.body.phone); //.replace(/[^\d/g,"");
+
+  //create a new user account using the phone number
+  admin
+    .auth()
+    .createUser({
+      uid: phone,
+    })
+    .then((user) => res.send(user))
+    .catch((err) => res.status(422).send({ error: err }));
+  //respond to the user saying that the client was made
 };

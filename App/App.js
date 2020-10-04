9b2b91461e99fb79aac42d10a0657d415ef62c26
alijb <alijb@post.bgu.ac.il>
2020-10-04 19:47:38 +0300

    Creating a firebase workspace for making fuctions to use in google clouds

--- App/App.js
+++ App/App.js
@@ -5,6 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     //one time password Authentication
+    //We need to setup A FireBase
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />

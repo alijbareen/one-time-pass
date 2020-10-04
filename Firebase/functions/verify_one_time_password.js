8ce0c2103f7401e9a8899e77d4829b0e179aed72
alijb <alijb@post.bgu.ac.il>
2020-10-05 01:27:18 +0300

    Verify

--- Firebase/functions/verify_one_time_password.js
+++ Firebase/functions/verify_one_time_password.js
@@ -1,8 +1,37 @@
+const { admin } = require("firebase-admin/lib/database");
+const { ref } = require("firebase-functions/lib/providers/database");
+
 module.exports = function (req, res) {
   if (!req.body.phone || !req.body.code) {
     return res.status(422).send({ error: "Phone and Code must be provided" });
   }
 
   const phone = String(req.body.phone).replace(/[^d]/g, "");
-  const code = parseInt(code);
+
+  const code = parseInt(req.body.code);
+
+  admin
+    .auth()
+    .getUser(phone)
+    .then(() => {
+      admin.database.ref("users/" + phone).on("value", (snapshot) => {
+        const user = snapshot.val();
+
+        if (user.code !== code || !user.codeValid) {
+          return res.status(422).send({ error: "Code not Valid" });
+        }
+
+        ref.update({ codeValid: false });
+
+        admin
+          .auth()
+          .createCustomToken(phone)
+          .then((token) => {
+            res.send({ token: token });
+          });
+      });
+    })
+    .catch((res) => {
+      res.status(422).send({ error: err });
+    });
 };

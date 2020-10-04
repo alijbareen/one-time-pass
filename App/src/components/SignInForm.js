51117d0db4e09accee1ea67d03fd76f063a83b99
alijb <alijb@post.bgu.ac.il>
2020-10-05 02:06:02 +0300

    SignInForm

---
+++ App/src/components/SignInForm.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import React, { Component } from "react";
+import { View, Text, TextInput } from "react-native";
+import { Button, FormLabel, FormInput } from "react-native-elements";
+
+const ROOT_URL =
+  "https://us-central1-one-time-password-b8290.cloudfunctions.net/";
+
+class SignInForm extends Component {
+  state = { phone: "", code: "" };
+
+  handleSubmit = async () => {
+    try {
+      const { phone, code } = this.state;
+      await axios.post(`${ROOT_URL}/verifyOneTimePassword`, { phone, code });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  render() {
+    return (
+      <View>
+        <View style={{ marginBottom: 10 }}>
+          <Text>Enter Your Phone Number</Text>
+          <TextInput
+            onChangeText={(phone) => this.setState({ phone })}
+            value={this.state.phone}
+            placeholder="phone"
+          />
+        </View>
+        <View style={{ marginBottom: 10 }}>
+          <Text>Enter Your Code</Text>
+          <TextInput
+            onChangeText={(code) => this.setState({ code })}
+            value={this.state.code}
+            placeholder="phone"
+          />
+        </View>
+        <Button onPress={this.handleSubmit.bind(this)} title="Submit" />
+      </View>
+    );
+  }
+}
+
+export default SignInForm;

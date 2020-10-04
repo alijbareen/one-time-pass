62e80b0b2d9a5161951b41fd010cf514d4e02944
alijb <alijb@post.bgu.ac.il>
2020-10-04 20:30:07 +0300

    Cleanup index

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -2,17 +2,5 @@ const functions = require("firebase-functions");
 
 const createUser = require("./create_user");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
-
-exports.goodbye = functions.https.onRequest((request, response) => {
-  response.send("Goodbye from Firebase!");
-});
-
 //function to create user
 exports.createUser = functions.https.onRequest(createUser);

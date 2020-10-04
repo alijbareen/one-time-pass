2f99a569e34d1eccf26b3ff4cc3b6e7ecddc1a13
alijb <alijb@post.bgu.ac.il>
2020-10-04 20:12:17 +0300

    setting up the functions in FireBase

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -1,5 +1,7 @@
 const functions = require("firebase-functions");
 
+const createUser = require("./create_user");
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -7,3 +9,10 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
 });
+
+exports.goodbye = functions.https.onRequest((request, response) => {
+  response.send("Goodbye from Firebase!");
+});
+
+//function to create user
+exports.createUser = functions.https.onRequest(createUser);

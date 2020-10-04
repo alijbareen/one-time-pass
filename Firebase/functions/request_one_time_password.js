03640b1366d460251d00b0f5d30ae80971f3a16b
alijb <alijb@post.bgu.ac.il>
2020-10-05 00:21:12 +0300

    CodeValid

--- Firebase/functions/request_one_time_password.js
+++ Firebase/functions/request_one_time_password.js
@@ -15,11 +15,25 @@ module.exports = function (req, res) {
     .then((userRecord) => {
       const code = Math.floor(Math.random() * 8999 + 1000);
 
-      twilio.messages.create({
-        body: "Your Code is " + code,
-        to: "+972" + phone,
-        from: "+17609907176",
-      });
+      twilio.messages.create(
+        {
+          body: "Your Code is " + code,
+          to: "+972" + phone,
+          from: "+17609907176",
+        },
+        (err) => {
+          if (err) {
+            return res.status(422).send({ error: err });
+          }
+
+          admin
+            .database()
+            .ref("users/" + phone)
+            .update({ code: code, codeValid: true }, () => {
+              res.send({ success: true });
+            });
+        }
+      );
     })
     .catch((err) => {
       res.status(422).send({ error: err });

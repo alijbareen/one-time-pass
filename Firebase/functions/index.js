75364a64d7367702a8998c27a4530e88e1b53104
alijb <alijb@post.bgu.ac.il>
2020-10-04 20:35:01 +0300

    Adding the serviceAccount to gitignore

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -1,6 +1,15 @@
+//service-access for all the app credintials
+//service-account allow us to edit the database
+const admin = require("firebase-admin");
+const serviceAccount = require("./service_account.json");
 const functions = require("firebase-functions");
 
 const createUser = require("./create_user");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://one-time-password-b8290.firebaseio.com",
+});
 
 //function to create user
 exports.createUser = functions.https.onRequest(createUser);

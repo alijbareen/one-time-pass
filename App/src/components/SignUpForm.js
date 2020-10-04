6c5468f3c60cf3208f36ae66c6ee2889ff6205d2
alijb <alijb@post.bgu.ac.il>
2020-10-05 01:56:00 +0300

    axios  :)))

--- App/src/components/SignUpForm.js
+++ App/src/components/SignUpForm.js
@@ -1,11 +1,25 @@
+import axios from "axios";
 import React, { Component } from "react";
 import { View, Text, TextInput } from "react-native";
 import { Button, FormLabel, FormInput } from "react-native-elements";
 
+const ROOT_URL =
+  "https://us-central1-one-time-password-b8290.cloudfunctions.net/";
+
 class SignUpForm extends Component {
   state = { phone: "" };
 
-  handleSubmit = () => {};
+  handleSubmit = () => {
+    axios
+      .post(`${ROOT_URL}/createUser`, {
+        phone: this.state.phone,
+      })
+      .then(() => {
+        axios.post(`${ROOT_URL}/requestOneTimePassword`, {
+          phone: this.state.phone,
+        });
+      });
+  };
 
   render() {
     return (

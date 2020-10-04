8c2129d303f104302d805941517b12dabc3a0006
alijb <alijb@post.bgu.ac.il>
2020-10-05 00:13:55 +0300

    Trying to Send Verifaction code as Text to the user

--- Firebase/functions/request_one_time_password.js
+++ Firebase/functions/request_one_time_password.js
@@ -1,14 +1,27 @@
 const admin = require("firebase-admin");
 
+const twilio = require("./twilio");
+
 module.exports = function (req, res) {
   if (!req.body.phone) {
     return res.status(422).send({ error: "Phone is Not Valid" });
   }
 
-  const phone = String(req.body.phone); //.replace(/[^\d]/g, "");
+  const phone = String(req.body.phone).replace(/[^\d]/g, "");
 
   admin
     .auth()
     .getUser(phone)
-    .then((userRecord) => {});
+    .then((userRecord) => {
+      const code = Math.floor(Math.random() * 8999 + 1000);
+
+      twilio.messages.create({
+        body: "Your Code is " + code,
+        to: "+972" + phone,
+        from: "+17609907176",
+      });
+    })
+    .catch((err) => {
+      res.status(422).send({ error: err });
+    });
 };

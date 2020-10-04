12e1e48c246d23822618c5744209141240e4702c
alijb <alijb@post.bgu.ac.il>
2020-10-05 01:21:19 +0300

    Validation and Verifying OTP

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -5,7 +5,7 @@ const serviceAccount = require("./service_account.json");
 const functions = require("firebase-functions");
 const request_one_time_password = require("./request_one_time_password");
 const createUser = require("./create_user");
-
+const verifyOneTimePassword = require("./verify_one_time_password");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://one-time-password-b8290.firebaseio.com",
@@ -15,4 +15,8 @@ admin.initializeApp({
 exports.createUser = functions.https.onRequest(createUser);
 exports.request_one_time_password = functions.https.onRequest(
   request_one_time_password
+);
+
+exports.verifyOneTimePassword = functions.https.onRequest(
+  verifyOneTimePassword
 );
